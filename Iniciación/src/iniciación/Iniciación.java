0d462c9e96bf1e775125da0a843e30cb0a94fd19
Estudiante <Estudiante@10.203.2.115>
2018-08-13 08:34:08 -0500

    Commit inicial

---
+++ Iniciación/src/iniciación/Iniciación.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iniciación;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Iniciación {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        for(int x=0;x<=8;x+=1){
+            System.out.println(x);
+        }
+    }
+
+}
